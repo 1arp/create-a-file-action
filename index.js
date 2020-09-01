2a92a37336073bd540fe3f1c5aaa93f22e652212
1arp <dekhijaegi@gmail.com>
2020-09-02 00:29:47 +0530

    basic functionality

---
+++ index.js
@@ -0,0 +1,27 @@
+const core = require('@actions/core');
+const fs = require('fs').promises;
+const path = require('path');
+
+async function createFile (){
+  try{
+    const filePath = core.getInput('path');
+    const file = core.getInput('file')
+    const content = core.getInput('content')
+    const absolutePath = path.join(process.cwd(),filePath)
+    try{
+      await fs.access(absolutePath)
+    }catch(error){
+      await fs.mkdir(absolutePath, {recursive: true})
+    }
+    try{
+      await fs.access(absolutePath)
+    }catch(error){
+      core.setFailed("couldn't create directory structure");
+    }
+    await fs.writeFile(path.join(absolutePath,file), content)
+  }catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+createFile();
